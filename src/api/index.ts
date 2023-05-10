d6b195756f39ebd187856b1c4812ad3ea5de435d
박서연 <seoyeonpub@gmail.com>
2023-05-10 22:53:35 +0900

    회원가입 api 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,10 +3,10 @@ import axios, { AxiosRequestConfig } from 'axios';
 const config: AxiosRequestConfig = {
   baseURL: 'http://www.ecog99.com/api/v1',
   timeout: 5000,
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-    Accept: 'application/json',
-  },
+  // headers: {
+  //   'Content-type': 'application/json; charset=UTF-8',
+  //   Accept: 'application/json',
+  // },
   withCredentials: true,
 };
 
